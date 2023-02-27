dcace31e1ba4fff6822a3045f0cbcfaabeed8113
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-02-27 23:01:33 +0530

    Create aliendictionary.cpp

---
+++ aliendictionary.cpp
@@ -0,0 +1,54 @@
+class Solution{
+    private:
+    vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	   int indegree[V]={0};
+	   for(int i=0;i<V;i++){
+	       for(auto it:adj[i]){
+	           indegree[it]++;
+	       }
+	   }
+	   queue<int>q;
+	  for(int i=0;i<V;i++){
+	      if(indegree[i]==0){
+	        q.push(i);  
+	      }
+	  } 
+	  vector<int>topo;
+	  while(!q.empty()){
+	      int node=q.front();
+	      q.pop();
+	      topo.push_back(node);
+	      //node is in your topo sort
+	      //pls remove it from topo
+	      for(auto it:adj[node]){
+	          indegree[it]--;
+	          if(indegree[it]==0) q.push(it);
+	      }
+	  }
+	  return topo;
+	}
+    public:
+    string findOrder(string dict[], int N, int K) {
+        vector<int>adj[K];
+        for(int i=0;i<N-1;i++){ // n-1 coz we need pairs
+          string s1=dict[i];
+          string s2=dict[i+1];
+          int len=min(s1.size(), s2.size()); // we need minimum coz for eg- s1='baa' s2='abcd' so we only compare minimum parts
+          for(int ptr=0;ptr<len; ptr++){
+              if(s1[ptr]!=s2[ptr]){
+              adj[s1[ptr]-'a'].push_back(s2[ptr]-'a'); // so now make the egdes between the characters if they are not same
+              break;   
+              }
+           }
+        }
+        vector<int>topo=topoSort(K, adj);
+        string ans="";
+        for(auto it:topo){
+           ans=ans+char(it+'a'); 
+        }
+        return ans;
+    }
+    
+    
+};
